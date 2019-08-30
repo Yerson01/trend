44899f795d228887aaa437ec45a69c9656a5e6d3
Yerson84 <yersonsoto84@gmail.com>
2019-08-30 19:08:36 -0400

    Creacion de la landing page

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/bf35a47ece.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Lexend+Exa|Montserrat+Alternates|Poppins:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>TrendStyle</title>
+</head>
+<body>
+    <header>
+        <div class="bar min-container">
+            <div class="logo-container">
+                <h3 class="logo">trend</h3>
+            </div><!--logo-container-->
+            <nav>
+                <ul class="nav-items">
+                    <li><a href="#" class="nav-item">Specs</a></li>
+                    <li><a href="#" class="nav-item">Products</a></li>
+                    <li><a href="#" class="nav-item">About</a></li>
+                </ul>
+            </nav><!--navbar-->
+            <div class="cart">
+                <a href="#" class="cart-container">
+                    <img src="img/svg/cart.svg" alt="Cart">
+                    <div class="cart-quantity">
+                        <span id="products-number">2</span>
+                    </div>
+                </a>
+            </div><!--cart-->
+        </div><!--bar.container-->
+
+        <div class="landing min-container">
+            <div class="introduction">
+                <div class="intro-text">
+                    <h1>Stylish wherever you are</h1>
+                    <p>Enjoy all the offers we have for you</p>
+                </div>
+                <div class="cta">
+                    <a href="#" class="btn btn-primary">Get Started</a>
+                    <a href="#" class="btn btn-secondary">Login <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="presentation">
+                <div class="cover">
+                    <img src="img/svg/cover.svg" alt="Cover">
+                </div>
+            </div>
+        </div>
+        <div class="landing-footer">
+            <img src="img/svg/vector1.svg" alt="" class="vector vector1">
+            <img src="img/svg/vector2.svg" alt="" class="vector vector2">
+            <img src="img/svg/vector3.svg" alt="" class="vector vector3">
+            <a href="#" class="down-arrow"><i class="fas fa-arrow-down"></i></a>
+        </div>
+    </header>
+</body>
+</html>
